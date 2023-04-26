ae97f7e8caaac7c62b05b5846376ffc1946fce58
SunnyDay <1246771571@qq.com>
2023-04-26 21:26:24 +0800

    基础review

---
+++ src/base/VarTest.kt
@@ -0,0 +1,19 @@
+package base
+
+/**
+ * Create by SunnyDay 2023/04/26 20:54:41
+ **/
+
+/* 注释从这里开始
+/* 包含嵌套的注释 */
+并且在这里结束。 */
+var test1 = 0
+const val test2 = 1
+class VarTest {
+
+}
+
+
+fun main (){
+
+}
