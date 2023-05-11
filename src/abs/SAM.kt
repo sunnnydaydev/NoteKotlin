3ee63d57741da5cd16b4eabc03f05f224695764a
SunnyDay <1246771571@qq.com>
2023-05-11 21:55:53 +0800

    SAM

--- src/abs/SAM.kt
+++ src/abs/SAM.kt
@@ -1,8 +1,23 @@
 package abs
 
+import java.util.ResourceBundle
+
 
 fun main() {
+    val uiState = object : UIState {
+        override fun isSelected(): Boolean {
+            return true
+        }
+    }
 
+    val uiState2 = UIState {
+       true
+    }
+}
+
+
+fun interface UIState {
+    fun isSelected(): Boolean
 }
 
 
