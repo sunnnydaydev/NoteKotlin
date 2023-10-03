4e070e97be41dfad98596fb95e085e327bff88ee
SunnyDay <1246771571@qq.com>
2023-10-03 18:01:26 +0800

    return and label ing

--- src/basic_type/Test1.kt
+++ src/basic_type/Test1.kt
@@ -1,9 +1,13 @@
 package basic_type
 
+
 /**
  * Create by SunnyDay 2023/10/03 17:00:11
  **/
 fun main() {
-    val str:String?=null
-    println(str.isNullOrBlank())
+    val list = arrayListOf(1,3,5,7,9)
+    list.forEach {
+        println("loop:${it}")
+        if (it==7)return
+    }
 }
