2f3a8077c12605f8eda0252eca388e6333bc0c6d
SunnyDay <1246771571@qq.com>
2023-10-05 12:51:51 +0800

    return and label finish

--- src/basic_type/Test1.kt
+++ src/basic_type/Test1.kt
@@ -5,9 +5,14 @@ package basic_type
  * Create by SunnyDay 2023/10/03 17:00:11
  **/
 fun main() {
-    val list = arrayListOf(1,3,5,7,9)
-    list.forEach {
-        println("loop:${it}")
-        if (it==7)return
+    val list = arrayListOf(1, 3, 5, 7, 9)
+
+    run runBlock@{
+        list.forEach(fun(value: Int) {
+            println("loop:${value}")
+            if (value == 7)
+                return@runBlock
+        })
     }
+    print("done with nested loop")
 }
