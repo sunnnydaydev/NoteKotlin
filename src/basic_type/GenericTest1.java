e022ae485aef06dc8d1e30b232cd901342417411
SunnyDay <1246771571@qq.com>
2023-10-09 22:02:27 +0800

    泛型 好难理解

--- src/basic_type/GenericTest1.java
+++ src/basic_type/GenericTest1.java
@@ -2,29 +2,34 @@ package basic_type;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Create by SunnyDay 2023/10/08 21:07:01
  **/
+class Person{}
+class Father extends Person{}
+class Child extends Father{}
+
 public class GenericTest1 {
     public static void main(String[] args) {
+        Collection<Person> myList1 = new MyList<>();
 
+        Collection<Father> myList2 = new MyList<>();
+
+        myList1.addAll(myList2);
     }
 }
 
 interface Collection<E>{
-    void addAll(Collection<? extends E> items);
-
+    void addAll(Collection<? super E> items);
 }
 
 class MyList<E> implements Collection<E>{
-
-    void copyAll(Collection<Object> to, Collection<String> from) {
-        to.addAll(from);
-    }
-
     @Override
-    public void addAll(Collection<? extends E> items) {
-
+    public void addAll(Collection<? super E> items) {
+    }
+    void copyAll(Collection<Object> to, Collection<String> from) {
+        from.addAll(to);
     }
 }
